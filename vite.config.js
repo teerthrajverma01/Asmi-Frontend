2b895d2694db846cbf33f2a08cbc86330975bcd9
Bharat Bhandari <bharatbhandari0302@gmail.com>
2024-03-24 18:30:24 +0530

    npm i taostify

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-toastify"], // Add this line to specify the external module
-    },
-  },
 });
